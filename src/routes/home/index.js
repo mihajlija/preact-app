8ab4278089d5833dc4fc22af74970fef2d020223
Milica <mihajlije@gmail.com>
2018-10-29 11:30:02 +0100

    add sailor moon to homeage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,12 @@
-import { h } from 'preact';
-import style from './style';
+import { h } from 'preact'
+import style from './style'
 
 const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-		<img scr="https://66.media.tumblr.com/99ac276098115a6317a6966e2e30ad39/tumblr_ph838qEHAb1vct3zyo1_640.png"/>
-	</div>
-);
+  <div class={style.home}>
+    <h1>Home</h1>
+    <p>This is the Home component.</p>
+    <img src='https://66.media.tumblr.com/99ac276098115a6317a6966e2e30ad39/tumblr_ph838qEHAb1vct3zyo1_640.png' />
+  </div>
+)
 
-export default Home;
+export default Home
