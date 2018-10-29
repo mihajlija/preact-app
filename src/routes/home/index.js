bd021f741c03ee27d5b721e1dc926506f4ca1028
Milica <mihajlije@gmail.com>
2018-10-29 11:17:56 +0100

    add image

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,6 +5,7 @@ const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
+		<img scr="https://66.media.tumblr.com/99ac276098115a6317a6966e2e30ad39/tumblr_ph838qEHAb1vct3zyo1_640.png"/>
 	</div>
 );
 
