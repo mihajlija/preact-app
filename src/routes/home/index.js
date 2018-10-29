7bc804cffbcb1312c85c31cda9b858eb1f7506d7
Milica <mihajlije@gmail.com>
2018-10-29 12:29:45 +0100

    add a greeting

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,7 +4,7 @@ import style from './style'
 const Home = () => (
   <div class={style.home}>
     <h1>Home</h1>
-    <p>This is the Home component.</p>
+    <p>Hello Sailor Moon</p>
     <img src='https://66.media.tumblr.com/99ac276098115a6317a6966e2e30ad39/tumblr_ph838qEHAb1vct3zyo1_640.png' />
   </div>
 )
